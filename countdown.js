c70212b27e074cc45ec6ec6bf39e1bcb4ba48295
muhammad haris <162983403+hony54320@users.noreply.github.com>
2024-06-30 00:31:15 +0500

    Add files via upload

---
+++ countdown.js
@@ -0,0 +1,22 @@
+import inquirer from 'inquirer';
+async function countdownTimer() {
+    const { minutes } = await inquirer.prompt([
+        {
+            type: 'number',
+            name: 'minutes',
+            message: 'Enter the number of minutes for the countdown:',
+        },
+    ]);
+    let seconds = minutes * 60;
+    const intervalId = setInterval(() => {
+        const minutesLeft = Math.floor(seconds / 60);
+        const secondsLeft = seconds % 60;
+        console.log(`Time Remaining: ${minutesLeft} minutes ${secondsLeft} seconds`);
+        seconds--;
+        if (seconds < 0) {
+            clearInterval(intervalId);
+            console.log('Countdown complete!');
+        }
+    }, 1000);
+}
+countdownTimer();
